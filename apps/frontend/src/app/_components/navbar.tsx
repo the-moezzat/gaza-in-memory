b67803cdbaab998f8f1e236175841f7880c77acb
Mohamed Ezzat <dev.moezzat@gmail.com>
2024-06-13 01:40:20 +0300

    feat: hero section

---
+++ apps/frontend/src/app/_components/navbar.tsx
@@ -0,0 +1,87 @@
+'use client'
+import React from 'react';
+import Link from "next/link";
+import {Globe, Plus} from "lucide-react";
+import {Button} from "@/components/ui/button";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+import {cn} from "@/lib/utils";
+import {usePathname} from "next/navigation";
+import Image from "next/image";
+
+export const appTabs = [
+    { name: "Home", relative: "", absoluteLink: "/" },
+    {
+        name: "Eye on Gaza",
+        relative: "eye-on-gaza",
+        absoluteLink: "/eye-on-gaza",
+    },
+    {
+        name: "Sound of Gaza",
+        relative: "sound-of-gaza",
+        absoluteLink: "/sound-of-gaza",
+    },
+    { name: "Shop", relative: "shop", absoluteLink: "/shop" },
+    {
+        name: "Boycott and supporters ",
+        relative: "boycott",
+        absoluteLink: "/boycott",
+    },
+];
+
+function Navbar() {
+    const pathname = usePathname();
+
+    pathname.split("/").at(1);
+
+    const activeLink =  appTabs.find((tab) => tab.relative === pathname.split("/").at(1));
+
+    return (
+        <nav className={"flex items-center justify-between border-b px-6 py-3 "}>
+            <Image
+                src={"/logo/logo.svg"}
+                width={80}
+                height={40}
+                alt={"Palestine Logo"}
+                className={"h-10"}
+            />
+
+            <ul className={"flex item-center gap-8"}>
+                {appTabs.map((link) => (
+                    <li key={link.name}>
+                        <Link
+                            href={link.absoluteLink}
+                            className={cn({
+                                "text-gray-900 font-medium": activeLink?.name === link.name,
+                                "text-gray-500": activeLink?.name !== link.name,
+                            })}
+                        >
+                            {link.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+
+            <div className={"text-gray-800 flex items-center gap-4"}>
+                <Button variant={"ghost"} size={"icon"}>
+                    <Globe size={18}/>
+                </Button>
+
+                <Button
+                    variant={"outline"}
+                    size={"sm"}
+                    className={"flex items-center gap-2"}
+                >
+                    <Plus/>
+                    <span>Add person</span>
+                </Button>
+
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png"/>
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
---
+++ apps/frontend/src/components/ui/flip-words.tsx
@@ -0,0 +1,91 @@
+"use client";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { AnimatePresence, motion, LayoutGroup } from "framer-motion";
+import { cn } from "@/lib/utils";
+
+export const FlipWords = ({
+  words,
+  duration = 3000,
+  className,
+}: {
+  words: string[] | undefined;
+  duration?: number;
+  className?: string;
+}) => {
+  const [currentWord, setCurrentWord] = useState(
+    words && words.length > 0 ? words[0] : "",
+  );
+  const [isAnimating, setIsAnimating] = useState<boolean>(false);
+
+  const startAnimation = useCallback(() => {
+    if (words && currentWord) {
+      const word = words[(words.indexOf(currentWord) ?? 0) + 1] || words[0];
+      setCurrentWord(word);
+      setIsAnimating(true);
+    }
+  }, [currentWord, words]);
+
+  useEffect(() => {
+    if (!isAnimating)
+      setTimeout(() => {
+        startAnimation();
+      }, duration);
+  }, [isAnimating, duration, startAnimation]);
+
+  return (
+    <AnimatePresence
+      onExitComplete={() => {
+        setIsAnimating(false);
+      }}
+    >
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: 10,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 0.4,
+          ease: "easeInOut",
+          type: "spring",
+          stiffness: 100,
+          damping: 10,
+        }}
+        exit={{
+          opacity: 0,
+          y: 0,
+          x: 0,
+          filter: "blur(8px)",
+          scale: 1,
+          position: "absolute",
+        }}
+        className={cn(
+          "z-10 inline-block relative text-left text-neutral-900 dark:text-neutral-100 px-2",
+          className,
+        )}
+        key={currentWord}
+      >
+        {(currentWord || "").split("").map((letter, index) => {
+          if (letter === " ") return letter;
+          return (
+            <motion.span
+              key={(currentWord || "") + index}
+              initial={{ opacity: 0, y: 10, filter: "blur(8px)" }}
+              animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+              transition={{
+                delay: index * 0.08,
+                duration: 0.4,
+              }}
+              className="inline-block"
+            >
+              {letter}
+            </motion.span>
+          );
+        })}
+      </motion.div>
+    </AnimatePresence>
+  );
+};
