d84f523b9c1bd319d08d27f33ed266885768582b
Mohamed Ezzat <dev.moezzat@gmail.com>
2024-10-11 04:46:06 +0300

    fix: webpack warining while build

--- apps/frontend/src/app/[locale]/page.tsx
+++ apps/frontend/src/app/[locale]/page.tsx
@@ -10,16 +10,18 @@ export function generateStaticParams() {
   return supportedLocales.map((locale) => ({ locale }));
 }
 
+export const revalidate = 900;
+
 export default function Home({ params }: { params: { locale: string } }) {
   return (
     <main className={"p-4"}>
-      <div className="h-[40rem] w-full rounded-xl flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden flex-col gap-20">
+      <div className="relative flex h-[40rem] w-full flex-col gap-20 overflow-hidden rounded-xl bg-black/[0.96] antialiased bg-grid-white/[0.02] md:items-center md:justify-center">
         <Spotlight
-          className="-top-40 left-0 md:left-60 md:-top-20"
+          className="-top-40 left-0 md:-top-20 md:left-60"
           fill="white"
         />
-        <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0 dark ">
-          <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+        <div className="dark relative z-10 mx-auto w-full max-w-7xl p-4 pt-20 md:pt-0">
+          <h1 className="bg-opacity-50 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-7xl">
             Not Just a Numbers <br />
             They Are Stories Unfinished
             {/* <FlipWords
@@ -40,8 +42,8 @@ export default function Home({ params }: { params: { locale: string } }) {
         <AnimatedQuoteBubbles /> {/* Add the new component here */}
       </div>
 
-      <section className={" mt-16 space-y-6"}>
-        <h2 className={"text-2xl text-gray-800 font-bold"}>Martyrs</h2>
+      <section className={"mt-16 space-y-6"}>
+        <h2 className={"text-2xl font-bold text-gray-800"}>Martyrs</h2>
         <div className={"flex gap-4"}>
           <MartyrCard />
           <MartyrCard />
