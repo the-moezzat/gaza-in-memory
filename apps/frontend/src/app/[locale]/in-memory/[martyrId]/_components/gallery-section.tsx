0ad5bc5de8a0616991ff63519c09044e1926b068
Mohamed Ezzat <dev.moezzat@gmail.com>
2024-10-21 00:08:47 +0300

    feat: showing social media accounts

--- apps/frontend/src/app/[locale]/in-memory/[martyrId]/_components/gallery-section.tsx
+++ apps/frontend/src/app/[locale]/in-memory/[martyrId]/_components/gallery-section.tsx
@@ -23,8 +23,7 @@ export default async function GallerySection({
     .from("gallery")
     .select("*")
     .eq("martyr_id", martyrId);
-  console.log(data);
-  console.log(error);
+
   return (
     <div className="space-y-4">
       <h2 className="text-xl font-semibold text-gray-800">Gallery</h2>
--- apps/frontend/src/app/[locale]/in-memory/[martyrId]/_components/profile-card.tsx
+++ apps/frontend/src/app/[locale]/in-memory/[martyrId]/_components/profile-card.tsx
@@ -3,11 +3,15 @@ import { calculateAge } from "@/app/[locale]/_utils/calculateAge";
 import { Separator } from "@/components/ui/separator";
 import { BadgeCheck, Medal } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import { socialMediaIconsMapper } from "../_utils/socialMediaIconsMapper";
 interface ProfileCardProps {
   martyr: Martyr;
 }
 
 export default function ProfileCard({ martyr }: ProfileCardProps) {
+  const socialMediaPlatforms = Object.keys(martyr.social_media ?? {});
+
   return (
     <div className="grid w-full grid-cols-[9fr,3fr] gap-4 rounded-2xl bg-white p-4 shadow-[0_0_10px_rgb(0,0,0,0.07)]">
       <div className="flex flex-col items-center gap-4">
@@ -33,6 +37,21 @@ export default function ProfileCard({ martyr }: ProfileCardProps) {
             <Medal size={16} />
             <span>Champion</span>
           </div>
+        </div>
+        <div className="flex items-center gap-4">
+          {socialMediaPlatforms.map((social) => (
+            <Link
+              href={
+                martyr.social_media?.[
+                  social as keyof typeof martyr.social_media
+                ] ?? ""
+              }
+              key={social}
+              className="text-gray-600"
+            >
+              {socialMediaIconsMapper(social, 18)}
+            </Link>
+          ))}
         </div>
       </div>
 
