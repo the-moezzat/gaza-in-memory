2c3fd297634c22c444c258e0e8e4559d5f9d6bf5
Mohamed Ezzat <dev.moezzat@gmail.com>
2024-10-21 23:22:17 +0300

    feat: add analytics and performance menotring vercel tool

--- apps/frontend/src/components/providers.tsx
+++ apps/frontend/src/components/providers.tsx
@@ -2,10 +2,14 @@ import { ReactNode } from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 import { MantineProvider } from "@mantine/core";
 import { Toaster } from "./ui/sonner";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function Providers({ children }: { children: ReactNode }) {
   return (
     <ClerkProvider>
+      <Analytics />
+      <SpeedInsights />
       <Toaster richColors />
       <MantineProvider>{children}</MantineProvider>
     </ClerkProvider>
