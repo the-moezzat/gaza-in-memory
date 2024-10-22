645d8bbfcc868e0000c7de93398757984e2fe763
Mohamed Ezzat <dev.moezzat@gmail.com>
2024-10-22 05:15:05 +0300

    refactor: change testimonial section to memory

---
+++ apps/frontend/src/app/[locale]/in-memory/[martyrId]/_components/memories-section.tsx
@@ -0,0 +1,89 @@
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import React from "react";
+import TestimonialCard from "./testimonial-card";
+import { createClerkSupabaseClientSsr } from "@/lib/client";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./grid-carousel";
+import ShareMemoryButton from "./share-memory-button";
+import { auth } from "@clerk/nextjs/server";
+
+interface TestimonialSectionProps {
+  martyrId: string;
+  martyrName: string;
+}
+
+export default async function TestimonialSection({
+  martyrId,
+  martyrName,
+}: TestimonialSectionProps) {
+  const client = createClerkSupabaseClientSsr(false);
+
+  const { userId } = auth();
+
+  console.log("userId", userId);
+
+  const { data: memories, error } = await client
+    .from("memories")
+    .select("*")
+    .eq("martyr_id", martyrId);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div className="flex w-full flex-col gap-4">
+      {memories.length > 0 ? (
+        <Carousel
+          className="max-w-full space-y-6"
+          opts={{
+            align: "start",
+            watchSlides: false,
+            // container: "div",
+            dragFree: true,
+            containScroll: false,
+          }}
+        >
+          <div className="flex items-center justify-between">
+            <h2 className="text-xl font-semibold text-gray-800">
+              {martyrName}&apos;s friends
+            </h2>
+            {memories.length > 0 && (
+              <div className="relative flex items-center gap-6">
+                <ShareMemoryButton />
+
+                <div className="flex items-center gap-2">
+                  <CarouselPrevious className="relative inset-0 -translate-y-0" />
+                  <CarouselNext className="relative inset-0 -translate-y-0" />
+                </div>
+              </div>
+            )}
+          </div>
+          <CarouselContent className="h-fit select-none">
+            {memories?.map((memory) =>
+              memory.content.length > 0 ? (
+                <CarouselItem key={memory.id} className="h-full w-full">
+                  <TestimonialCard memory={memory} />
+                </CarouselItem>
+              ) : null,
+            )}
+          </CarouselContent>
+        </Carousel>
+      ) : (
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h2 className="text-lg font-semibold text-gray-700">
+            There are no memories yet. Be the first to share your memory about{" "}
+            {martyrName}.
+          </h2>
+          <ShareMemoryButton />
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ apps/frontend/src/app/[locale]/in-memory/[martyrId]/_components/memory-card.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { clerkClient } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { format } from "date-fns";
+import { Memory } from "../_types/Memory";
+import MemoryChainCarousel from "./memory-chain-carousel";
+
+interface TestimonialCardProps {
+  memory: Memory;
+}
+
+export default async function TestimonialCard({
+  memory,
+}: TestimonialCardProps) {
+  const user = await clerkClient().users.getUser(memory.author_id!);
+
+  return (
+    <div className="grid w-full grid-cols-[auto,1fr] grid-rows-[auto,1fr] gap-x-4 gap-y-4 rounded-lg border p-4">
+      <Image
+        src={user.imageUrl}
+        alt={user.fullName ?? ""}
+        width={60}
+        height={60}
+        className="col-start-1 row-span-2 row-start-1 h-12 w-12 rounded-full object-cover"
+      />
+      <div className="">
+        <p className="font-medium text-gray-800">
+          {user.fullName}{" "}
+          <span className="text-sm font-normal text-gray-500">
+            ({memory.relationship})
+          </span>
+        </p>
+        <p className="text-sm text-gray-500">
+          {format(memory.created_at!, "dd MMM yyyy")}
+        </p>
+      </div>
+      <div className="self-start justify-self-stretch">
+        <MemoryChainCarousel memories={memory.content} />
+      </div>
+      {/* <p className="leading-relaxed text-gray-700">{memory.content}</p> */}
+    </div>
+  );
+}
