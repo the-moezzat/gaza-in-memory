537d433c4596c45d4b6fc5d4bbc837a10acdc056
Mohamed Ezzat <dev.moezzat@gmail.com>
2024-11-02 02:09:20 +0200

    feat: interests section localization

--- apps/frontend/src/app/[locale]/in-memory/[martyrId]/_components/interest-section.tsx
+++ apps/frontend/src/app/[locale]/in-memory/[martyrId]/_components/interest-section.tsx
@@ -8,6 +8,8 @@ import {
 } from "./memorySection/grid-carousel";
 import { createClerkSupabaseClientSsr } from "@/lib/client";
 import React from "react";
+import { getCurrentLocale } from "@/utils/getLocaleServer";
+import translator from "../_glossary/translator";
 
 export default async function InterestSection({ martyr }: { martyr: Martyr }) {
   const client = createClerkSupabaseClientSsr(false);
@@ -16,27 +18,35 @@ export default async function InterestSection({ martyr }: { martyr: Martyr }) {
     .select("*")
     .eq("martyr_id", martyr.id);
 
+  const locale = getCurrentLocale();
+  const t = translator(locale);
   return (
     <div className="w-full">
       <Carousel
         className="w-full space-y-2 md:space-y-4 lg:space-y-6"
-        opts={{ loop: false, dragFree: true, align: "start" }}
+        opts={{
+          loop: false,
+          dragFree: true,
+          align: "start",
+          direction: locale === "ar" ? "rtl" : "ltr",
+        }}
       >
         <div className="flex items-center justify-between gap-2">
           <h2 className="text-lg font-semibold text-gray-800 md:text-xl">
-            What{" "}
+            {t.interests({ name: martyr.first_name })}
+            {/* What{" "}
             {martyr.first_name.charAt(0).toUpperCase() +
               martyr.first_name.slice(1)}{" "}
-            loved and cared about
+            loved and cared about */}
           </h2>
 
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 rtl:flex-row-reverse">
             <CarouselPrevious className="relative inset-0 -translate-y-0" />
             <CarouselNext className="relative inset-0 -translate-y-0" />
           </div>
         </div>
 
-        <CarouselContent className="w-full auto-cols-[83%] pl-2 md:auto-cols-[55%] lg:auto-cols-[35%] lg:pl-4">
+        <CarouselContent className="w-full select-none auto-cols-[83%] pl-2 md:auto-cols-[55%] lg:auto-cols-[35%] lg:pl-4">
           {data?.map((interest) => (
             <CarouselItem key={interest.id} className="pl-2 lg:pl-4">
               <div className="space-y-4 rounded-xl border p-4">
